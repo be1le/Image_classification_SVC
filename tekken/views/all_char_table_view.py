cae77a655529739bf9323b5bcac0c085c5643817
seonghyeon <68859996+bae-code@users.noreply.github.com>
2022-01-21 11:05:20 +0900

    delete -mongodb index ,id,pw

--- tekken/views/all_char_table_view.py
+++ tekken/views/all_char_table_view.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Blueprint
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.0pi7g.mongodb.net/Cluster0?retryWrites=true&w=majority')  # 클라이언트 설정시 작성
+client = MongoClient('')  # 클라이언트 설정시 작성
 db = client.TEKKEN
 
 
